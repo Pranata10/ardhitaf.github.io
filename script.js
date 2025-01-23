4e261441ca8be3b53c6c132d6856a4adeacac4ae
Pranata10 <160474907+Pranata10@users.noreply.github.com>
2025-01-23 10:12:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    document.querySelectorAll('.animate-on-scroll').forEach(img => {
+        observer.observe(img);
+    });
+});
